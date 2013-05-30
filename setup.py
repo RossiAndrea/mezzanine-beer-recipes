9310ab0df33b5f29363e4da984921720c65cc7c4
Andrea Rossi <direlemming+github@gmail.com>
2013-05-30 14:04:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import setup, find_packages
 from mezzanine_recipes import __version__
 
 setup(
-    name='mezzanine-recipes',
+    name='mezzanine-beer-recipes',
     version=__version__,
-    author='Thomas Jetzinger',
-    author_email='thomas@jetzinger.com',
-    description='Recipe blog posts for Mezzanine CMS',
-    url='https://github.com/tjetzinger/mezzanine-recipes',
+    author='Andrea Rossi',
+    author_email='direlemming+github@gmail.com',
+    description='Beers Recipe blog posts for Mezzanine CMS',
+    url='https://github.com/RossiAndrea/mezzanine-beer-recipes',
     packages=find_packages(),
     include_package_data=True,
     setup_requires=[
         'setuptools_git>=0.3',
     ],
     install_requires=[
-        'Mezzanine==1.3.0',
-        'django-tastypie==0.9.12-alpha',
-        'South==0.7.6',
-        'uuid==1.30',
+        'Mezzanine>=1.3.0',
+        'django-tastypie>=0.9.12-alpha',
+        'South>=0.7.6',
+        'uuid>=1.30',
     ],
-    dependency_links = [
-        'http://github.com/toastdriven/django-tastypie/tarball/master#egg=django-tastypie-0.9.12-alpha',
-    ],
+    # dependency_links = [
+    #     'http://github.com/toastdriven/django-tastypie/tarball/master#egg=django-tastypie-0.9.12-alpha',
+    # ],
     classifiers = [
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
