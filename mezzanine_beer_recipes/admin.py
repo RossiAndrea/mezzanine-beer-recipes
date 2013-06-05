49e08e4f380a9c56c1c13aabb949a98c044203f8
RossiAndrea <direlemming+github@gmail.com>
2013-06-05 15:10:31 +0200

    Fix & Tidying on `admin.py`

--- mezzanine_beer_recipes/admin.py
+++ mezzanine_beer_recipes/admin.py
@@ -3,24 +3,26 @@ from copy import deepcopy
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-#from django.conf import settings
+# from django.conf import settings
 
 from mezzanine.blog.admin import BlogPostAdmin as MezzanineBlogPostAdmin
-from mezzanine.blog.models import BlogPost as MezzanineBlogPost
+# from mezzanine.blog.models import BlogPost as MezzanineBlogPost
 from mezzanine.core.admin import TabularDynamicInlineAdmin
 from mezzanine.conf import settings
 
 from tastypie.admin import ApiKeyInline
 from tastypie.models import ApiAccess
 
-from .models import Recipe, Ingredient, WorkingHours, CookingTime, RestPeriod, BlogPost
+from .models import Recipe, Ingredient, Mashing, Boiling, Fermentation
+# from .models import BlogPost
 from .models import Recipe_Ingredients, Ingredient_Type
 
 
 blogpost_fieldsets = deepcopy(MezzanineBlogPostAdmin.fieldsets)
 blogpost2_fieldsets = deepcopy(MezzanineBlogPostAdmin.fieldsets)
-blogpost_fieldsets[0][1]["fields"].extend(["summary", "liters", "difficulty", "source",
-                                           "original_gravity", "final_gravity", "ibu_bitter",
+blogpost_fieldsets[0][1]["fields"].extend(["summary", "liters", "difficulty",
+                                           "source", "original_gravity",
+                                           "final_gravity", "ibu_bitter",
                                            "color", "alchol"])
 blogpost2_fieldsets[0][1]["fields"].extend(["modified_date"])
 recipe_list_display = deepcopy(MezzanineBlogPostAdmin.list_display)
@@ -29,38 +31,51 @@ if not settings.BLOG_USE_FEATURED_IMAGE:
     blogpost_fieldsets[0][1]["fields"].insert(-6, "featured_image")
     recipe_list_display.insert(0, "admin_thumb")
 
-class WorkingHoursInline(admin.TabularInline):
-    model = WorkingHours
 
-class CookingTimeInline(admin.TabularInline):
-    model = CookingTime
+class MashingInline(admin.TabularInline):
+    model = Mashing
 
-class RestPeriodInline(admin.TabularInline):
-    model = RestPeriod
-    fields = ("days", "hours", "minutes",)
+
+class BoilingInline(admin.TabularInline):
+    model = Boiling
+
+
+class FermentationInline(admin.TabularInline):
+    model = Fermentation
+    fields = ("days", )
+
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+
+class IngredientTypeAdmin(admin.ModelAdmin):
+    model = Ingredient_Type
+
+
 class RecipeIngredientsAdminInLine(TabularDynamicInlineAdmin):
     model = Recipe_Ingredients
+
 
 class RecipeAdmin(MezzanineBlogPostAdmin):
     """
     Admin class for recipes.
     """
-    inlines = (RecipeIngredientsAdminInLine, WorkingHoursInline, CookingTimeInline, RestPeriodInline,)
+    inlines = (RecipeIngredientsAdminInLine, MashingInline, BoilingInline,
+               FermentationInline,)
     fieldsets = blogpost_fieldsets
     list_display = recipe_list_display
+
 
 class BlogPostAdmin(MezzanineBlogPostAdmin):
     fieldsets = blogpost2_fieldsets
 
 
-admin.site.unregister(MezzanineBlogPost)
-admin.site.register(BlogPost, BlogPostAdmin)
+# admin.site.unregister(MezzanineBlogPost)
+# admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Ingredient_Type, IngredientTypeAdmin)
 
 
 class UserModelAdmin(UserAdmin):
